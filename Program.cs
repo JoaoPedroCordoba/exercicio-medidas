28a8dd7169606b6c74afcde377e169864c05b527
JoaoPedroCordoba <jp.8pedro86@gmail.com.br>
2022-03-21 03:14:20 -0300

    finalizao exercicio

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double  cm, m, km;
+
+Console.WriteLine("Digite aqui os Metros(m), por favor");
+m = Convert.ToDouble(Console.ReadLine());
+
+cm = m * 100;
+km = m / 1000;
+
+Console.WriteLine($"Centimetros:{cm}cm");
+Console.WriteLine($"Metros:{m}m");
+Console.WriteLine($"Quilometros:{km}km");
